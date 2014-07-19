409544aa41eb0fcbee7fd76a38bde0f7245f4b56
Adam Yang <yangmm.adam@gmail.com>
2014-07-19 09:32:14 +0800

    创建账户管理界面

---
+++ M-World/MWorld/src/com/mworld/ui/login/AccountActivity.java
@@ -0,0 +1,16 @@
+package com.mworld.ui.login;
+
+import com.mworld.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccountActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.accountactivity_layout);
+	}
+
+}
