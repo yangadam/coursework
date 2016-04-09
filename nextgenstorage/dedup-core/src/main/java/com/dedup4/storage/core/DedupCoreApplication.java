a52b828037d1a4a9b3fdfede53322c1333fc1251
Adam Yang <yangmm.adam@gmail.com>
2016-04-09 23:26:12 +0800

    Minor changes

--- nextgenstorage/dedup-core/src/main/java/com/dedup4/storage/core/DedupCoreApplication.java
+++ nextgenstorage/dedup-core/src/main/java/com/dedup4/storage/core/DedupCoreApplication.java
@@ -1,7 +1,9 @@
 package com.dedup4.storage.core;
 
+import com.dedup4.storage.common.util.MessageSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
@@ -13,6 +15,9 @@ public class DedupCoreApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DedupCoreApplication.class);
 
+    @Autowired
+    private MessageSender messageSender;
+
     public static void main(String[] args) {
         SpringApplication.run(DedupCoreApplication.class, args);
     }
@@ -21,6 +26,7 @@ public class DedupCoreApplication {
     public void receiveDefault(String text) {
         LOGGER.info(text);
         // TODO deduplicate file
+        messageSender.send("queue.dedup.web", text);
     }
 
 }
