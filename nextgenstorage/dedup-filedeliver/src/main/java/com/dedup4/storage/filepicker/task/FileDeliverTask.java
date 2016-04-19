c0047745e214506e3b36f9f5b2c4a3cd7d8661bd
Adam Yang <yangmm.adam@gmail.com>
2016-04-19 17:31:59 +0800

    Some changes for dedup-core.

--- nextgenstorage/dedup-filedeliver/src/main/java/com/dedup4/storage/filepicker/task/FileDeliverTask.java
+++ nextgenstorage/dedup-filedeliver/src/main/java/com/dedup4/storage/filepicker/task/FileDeliverTask.java
@@ -5,6 +5,7 @@ import com.dedup4.storage.filepicker.util.SshHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,9 @@ public class FileDeliverTask {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FileDeliverTask.class);
 
+    @Value("${dedup.upload.path}")
+    private String path;
+
     @Autowired
     private SshHelper sshHelper;
 
@@ -28,13 +32,13 @@ public class FileDeliverTask {
     public void deliverFiles() {
         LOGGER.info("Start to deliver files.");
         File root = (File.listRoots())[0];
-        String path = root.getAbsolutePath() + "tmp/dedup/upload";
-        File folder = new File(path);
+        String absPath = root.getAbsolutePath() + path;
+        File folder = new File(absPath);
         if (folder.isDirectory()) {
             File[] files = folder.listFiles();
             for (File file : files != null ? files : new File[0]) {
                 if (file.isFile()) {
-                    sshHelper.upload(folder.toPath(), "/tmp/dedup/upload", file.getName());
+                    sshHelper.upload(folder.toPath(), '/' + path, file.getName());
                     // Send a message to deduplication server
                     messageSender.send("queue.filedeliver.dedup", file.getName());
                 }
