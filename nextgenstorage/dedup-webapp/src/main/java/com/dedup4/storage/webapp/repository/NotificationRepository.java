0c863db66e7147a1c3059b3c5d269abedf819651
Adam Yang <yangmm.adam@gmail.com>
2016-05-18 13:42:23 +0800

    Remove unused method.

--- nextgenstorage/dedup-webapp/src/main/java/com/dedup4/storage/webapp/repository/NotificationRepository.java
+++ nextgenstorage/dedup-webapp/src/main/java/com/dedup4/storage/webapp/repository/NotificationRepository.java
@@ -3,12 +3,8 @@ package com.dedup4.storage.webapp.repository;
 import com.dedup4.storage.webapp.domain.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Collection;
-import java.util.List;
-
 /**
  * @author Yang Mengmeng Created on Mar 29, 2016.
  */
 public interface NotificationRepository extends MongoRepository<Notification, String> {
-    List<Notification> findByUsernameToIn(Collection<String> collection);
 }
